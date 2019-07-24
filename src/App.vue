e1079da9a1ae1420e89d336b19b3c277e571663b
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-07-24 11:09:34 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <span>Title</span>
+    </header>
+    <v-fade-transition mode="out-in">
+      <router-view/>
+    </v-fade-transition>
   </div>
 </template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+export default class App extends Vue {}
+</script>
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
@@ -22,4 +27,21 @@
     color #2c3e50
     &.router-link-exact-active
       color #42b983
+header
+  margin 0
+  height 56px
+  padding 0 16px 0 24px
+  background-color #67b2e7
+  color #ffffff
+
+header span
+  display block
+  position relative
+  font-size 25px
+  line-height 1
+  letter-spacing .02em
+  font-weight 700
+  box-sizing border-box
+  padding-top 16px
+  text-align left
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <v-text-field
+      label="test"
+      placeholder="aiueo"/>
   </div>
 </template>
 
